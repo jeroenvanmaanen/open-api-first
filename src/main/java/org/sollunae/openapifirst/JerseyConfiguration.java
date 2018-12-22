c366c7541906beddf2415b3db243bbaa4d27751b
Jeroen van Maanen <jeroen@leialearns.org>
2018-12-22 10:36:37 +0100

    Initial commit.

---
+++ src/main/java/org/sollunae/openapifirst/JerseyConfiguration.java
@@ -0,0 +1,8 @@
+package org.sollunae.openapifirst;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JerseyConfiguration extends ResourceConfig {
+}
