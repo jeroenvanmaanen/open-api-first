046a3176e049464b7ed1443fdb0446a22636290f
Jeroen van Maanen <jeroen@leialearns.org>
2018-12-22 10:37:38 +0100

    Initial commit.

---
+++ src/main/java/org/sollunae/openapifirst/OpenApiFirstApplication.java
@@ -0,0 +1,14 @@
+package org.sollunae.openapifirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiFirstApplication.class, args);
+	}
+
+}
+
